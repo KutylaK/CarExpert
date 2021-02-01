54b027878d24a9120d2d7a9c3f88e387060edd40
pdziadosz <39157602+pdziadosz@users.noreply.github.com>
2021-02-01 15:29:48 +0100

    Update CarExpert.App.py

--- CarExpert.App/CarExpert.App/CarExpert.App.py
+++ CarExpert.App/CarExpert.App/CarExpert.App.py
@@ -1,3 +1,6 @@
+from tkinter.ttk import Combobox
+import tkinter as tk
+import tkinter
 from tkinter import *
 import pandas as pd
 import numpy as np
@@ -9,6 +12,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.cluster import KMeans
 from sklearn.neighbors import KNeighborsClassifier
+
 
 l1=[
 'rozrusznik nie kręci','światła się nie palą','samochód stał na mrozie','brak powietrza w kole','niski poziom oleju','biegi nie chcą się zmieniać','sprzęgło lekko się wciska','samochód zjeżdza z górki','głośny silnik','stuknięcia na górkach I dołkach','słaba siła hamowania','słaba siła silnika','wieksze zuzycie paliwa','miękki pedał hamulca','spryskiwacze nie działaja','wysoka temperatura silnika','samochód gaśnie'
@@ -58,7 +62,6 @@ _ = tree.plot_tree(sampleTree, feature_names=l1, filled=True, rounded=True)
 plt.savefig('branch.png', bbox_inches='tight')
 
 
-
 print("K-sąsiadów")
 print()
 
@@ -90,69 +93,55 @@ def x_values():
     l2 = [0] * len(l1)
     t1.delete("1.0", END)
 
-    x_values = [Symptom1.get(), Symptom2.get(), Symptom3.get()]
+    x_values = [cb1.current(), cb2.current(), cb3.current()]
 
     for k in range(0, len(l2)):
-        if l1[k] in x_values:
+        if k in x_values:
             l2[k] = 1
-
+    print(l2)
     return [l2]
 
+window = Tk()
+window.title("System ekspercki - mechanik")
+window.geometry("550x225")
 
-root = Tk()
-root.configure(background='gray')
+label1 = Label(window, text="Objaw 1.")
+label1.grid(row=6, column=2, padx=10, pady=(20, 0))
 
-Symptom1 = StringVar()
-Symptom1.set(None)
-Symptom2 = StringVar()
-Symptom2.set(None)
-Symptom3 = StringVar()
-Symptom3.set(None)
+label2 = Label(window, text="Objaw 2.")
+label2.grid(row=8, column=2, pady=(20, 0))
 
+label3 = Label(window, text="Objaw 3.")
+label3.grid(row=10, column=2, pady=(20, 0))
 
+btn1 = Button(window, text="Zdiagnozuj drzewem decyzyjnym", command=drzewo, bg="gold", fg="black")
+btn1.grid(row=6, column=6, padx=40, pady=(20, 0))
 
-w2 = Label(root, justify=CENTER, text="System ekspercki - Mechanik", fg="white", bg="gray")
+btn2 = Button(window, text="Zdiagnozuj lasem losowym", command=las_losowy, bg="gold", fg="black")
+btn2.grid(row=8, column=6, pady=(20, 0))
 
-w2.grid(row=1, column=0, columnspan=2, padx=100)
+btn3 = Button(window, text="Zdiagnozuj k-sąsiadów", command=k_sasiadow, bg="gold", fg="black")
+btn3.grid(row=10, column=6, pady=(20, 0))
 
-S1Lb = Label(root, text="Objaw 1", fg="yellow", bg="black")
-S1Lb.grid(row=7, column=0, pady=10, sticky=W)
+cb1 = Combobox(window, width=30)
+cb1['values'] = l1
+cb1.grid(row=6, column=3, pady=(20, 0))
 
-S2Lb = Label(root, text="Objaw 2", fg="yellow", bg="black")
-S2Lb.grid(row=8, column=0, pady=10, sticky=W)
+cb2 = Combobox(window, width=30)
+cb2['values'] = l1
+cb2.grid(row=8, column=3, pady=(20, 0))
 
-S3Lb = Label(root, text="Objaw 3", fg="yellow", bg="black")
-S3Lb.grid(row=9, column=0, pady=10, sticky=W)
+cb3 = Combobox(window, width=30)
+cb3['values'] = l1
+cb3.grid(row=10, column=3, pady=(20, 0))
 
+t1 = Text(window, height=1, width=35, bg="green yellow", fg="black", font="none 12 bold")
+t1.grid(row=17, column=3, columnspan=6)
 
-lrLb = Label(root, text="Diagnoza", fg="white", bg="red")
-lrLb.grid(row=15, column=0, pady=10,sticky=W)
+label4 = Label(window, text="Decyzja:")
+label4.grid(row=16, column=3, columnspan=6)
 
+label5 = Label(window)
+label5.grid(row=15, column=3, columnspan=6)
 
-# entries
-OPTIONS = sorted(l1)
-
-S1En = OptionMenu(root, Symptom1,*OPTIONS)
-S1En.grid(row=7, column=1)
-
-S2En = OptionMenu(root, Symptom2,*OPTIONS)
-S2En.grid(row=8, column=1)
-
-S3En = OptionMenu(root, Symptom3,*OPTIONS)
-S3En.grid(row=9, column=1)
-
-
-dst = Button(root, text="Zdiagnozuj drzewem decyzyjnym", command=drzewo, bg="green", fg="yellow")
-dst.grid(row=8, column=3)
-
-dst = Button(root, text="Zdiagnozuj lasem losowym", command=las_losowy, bg="green", fg="yellow")
-dst.grid(row=9, column=3)
-
-dst = Button(root, text="Zdiagnozuj k-sąsiadów", command=k_sasiadow, bg="green", fg="yellow")
-dst.grid(row=10, column=3)
-
-
-t1 = Text(root, height=1, width=40,bg="orange",fg="black")
-t1.grid(row=15, column=1, padx=10)
-
-root.mainloop()
+window.mainloop()
